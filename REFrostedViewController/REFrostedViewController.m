6a9a8e35a64fc6bd09e89d42704995218609e164
maybe <suntianwei@msn.com>
2013-10-17 02:20:05 +0800

    Update REFrostedViewController.m

--- REFrostedViewController/REFrostedViewController.m
+++ REFrostedViewController/REFrostedViewController.m
@@ -62,7 +62,7 @@
 
 - (void)commonInit
 {
-    self.wantsFullScreenLayout = YES;
+    // self.wantsFullScreenLayout = YES;
     _animationDuration = 0.35f;
     _blurTintColor = REUIKitIsFlatMode() ? nil : [UIColor colorWithWhite:1 alpha:0.75f];
     _blurSaturationDeltaFactor = 1.8f;
